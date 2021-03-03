c45fe3dd5420a80e5e3bdd1321a0a2f23639fa77
Samarjit Debnath <contactsamarjit100@gmail.com>
2021-03-04 01:43:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    let date = new Date();
+    let htime = date.getHours();
+    let mtime = date.getMinutes();
+    let stime = date.getSeconds();
+
+    let hrotation = ((30 * htime) + (mtime / 2) + (stime/120));
+    let mrotation = 6 * mtime + stime / 10;
+    let srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    min.style.transform = `rotate(${mrotation}deg)` ;
+    sec.style.transform = `rotate(${srotation}deg)` ;
+    
+
+}, 1000);
+
